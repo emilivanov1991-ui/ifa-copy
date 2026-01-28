b0ee28b7581b91b7dca5858c677674ef7d0ebbf1
Емил Иванов <emil.ivanov1991@gmail.com>
2026-01-28 14:37:59 +0000

    File changes

--- src/components/financial-plan/FinancialPlanPresentation.jsx
+++ src/components/financial-plan/FinancialPlanPresentation.jsx
@@ -203,7 +203,7 @@ export default function FinancialPlanPresentation({ planData, clientData, analys
                 stroke="#64748b"
               />
               <YAxis 
-                label={{ value: 'Капитал (%)', angle: -90, position: 'insideLeft' }}
+                label={{ value: 'Капитал (хил. лв)', angle: -90, position: 'insideLeft' }}
                 stroke="#64748b"
               />
               <Tooltip 

dd7aac7b967a42e7acad54667049ac29c43c8959
Емил Иванов <emil.ivanov1991@gmail.com>
2026-02-17 23:01:27 +0000

    File changes

--- src/components/analysis/ReserveStep.jsx
+++ src/components/analysis/ReserveStep.jsx
@@ -382,19 +382,14 @@ export default function ReserveStep({ data, onChange, showErrors, plannerData })
                     className={`rounded-lg text-center ${isFieldInvalid(data.partner_checking_account) ? 'border-red-500 bg-red-50' : ''}`}
                     required
                   />
-                  <Select 
-                    value={data.partner_checking_account_bank || ''} 
+                  <Combobox
+                    options={BANK_OPTIONS}
+                    value={data.partner_checking_account_bank || ''}
                     onValueChange={(value) => onChange('partner_checking_account_bank', value)}
-                  >
-                    <SelectTrigger className="rounded-lg text-sm [&>span]:text-center [&>span]:w-full">
-                      <SelectValue placeholder="Изберете" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {BANK_OPTIONS.map(bank => (
-                        <SelectItem key={bank.value} value={bank.value}>{bank.label}</SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
+                    placeholder="Банка"
+                    searchPlaceholder="Търси банка..."
+                    emptyText="Няма намерена банка."
+                  />
                 </div>
 
                 {/* Спестовна сметка */}

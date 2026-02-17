8ad79adb0a333dfd2376666f6d060e4df4583873
Емил Иванов <emil.ivanov1991@gmail.com>
2026-02-17 23:00:16 +0000

    File changes

--- src/components/analysis/ReserveStep.jsx
+++ src/components/analysis/ReserveStep.jsx
@@ -248,19 +248,14 @@ export default function ReserveStep({ data, onChange, showErrors, plannerData })
                   onChange={(e) => onChange('client_savings_account', parseInt(e.target.value) || 0)}
                   className="rounded-lg text-center"
                 />
-                <Select 
-                  value={data.client_savings_account_bank || ''} 
+                <Combobox
+                  options={BANK_OPTIONS}
+                  value={data.client_savings_account_bank || ''}
                   onValueChange={(value) => onChange('client_savings_account_bank', value)}
-                >
-                  <SelectTrigger className="rounded-lg text-sm [&>span]:text-center [&>span]:w-full">
-                    <SelectValue placeholder="Изберете" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    {BANK_OPTIONS.map(bank => (
-                      <SelectItem key={bank.value} value={bank.value}>{bank.label}</SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
+                  placeholder="Банка"
+                  searchPlaceholder="Търси банка..."
+                  emptyText="Няма намерена банка."
+                />
               </div>
 
               {/* Срочен депозит */}

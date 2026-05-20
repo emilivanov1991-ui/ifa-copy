f91d6043b5c204c2c946076651d9d47f129ce8f9
Емил Иванов <emil.ivanov1991@gmail.com>
2026-05-20 12:49:29 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
+import UsefulInfo from './pages/UsefulInfo';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
@@ -60,6 +61,7 @@ const AuthenticatedApp = () => {
           }
         />
       ))}
+      <Route path="/usefulinfo" element={<LayoutWrapper currentPageName="UsefulInfo"><UsefulInfo /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
